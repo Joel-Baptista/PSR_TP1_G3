a58970421a6b5e2f5bb91255d1f526f6b2cf9526
tiago <tiagomateus.fonte13@ua.pt>
2021-10-30 12:40:30 +0100

    Escolha do modo do jogo + primeira parte do jogo sem tempo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import argparse
+import readchar
+import random
+import string
 
 def main():
     parser = argparse.ArgumentParser(description='Escolha do modo de jogo.')
@@ -18,8 +21,20 @@ def main():
         withTime(args)
 
 def withoutTime(args):
+    number_of_hits = 0
+    number_of_types = 0
+    for i in range(args['maximum_value']):
+        random_char = random.choice(string.ascii_letters)
+        print('Type ' + str(random_char))
+        pressed_char = readchar.readkey()
+        if random_char == pressed_char:
+            print('You typed ' + pressed_char + '. ' + 'Correct!')
+            number_of_hits += 1
+            number_of_types += 1
+        else:
+            print('You typed ' + pressed_char + '. ' + 'Wrong!')
+            number_of_types += 1
 
-def withTime(args):
 
 
 if __name__ == '__main__':
